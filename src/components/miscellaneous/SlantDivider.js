bab9cedde00c01c0b5dadc6e80ba5627d3b85f45
gjcooper99 <gjcooper99@gmail.com>
2022-03-18 21:55:26 -0700

    Refactor SlantBanner into Separate Component

---
+++ src/components/miscellaneous/SlantDivider.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SlantDivider = () => {
+  return (
+    <div className='content-text'>
+        <h1>{this.heading}</h1>
+        <p>{this.par1}</p>
+        <p>{this.par2}</p>
+        <p>{this.par3}</p>
+      <div className='content-links'>
+        <p>Boogas</p>
+      </div>
+      <div className='content-lower'/>
+    </div>
+  )
+}
+
+export default SlantDivider
