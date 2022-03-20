85e2ee17a73995e2b441274ffb90c418c22e96aa
gjcooper99 <gjcooper99@gmail.com>
2022-03-19 19:49:39 -0700

    Added parallax effect to top part of space banner

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// eslint-disable-next-line
 import { Col, Row } from 'react-bootstrap';
 
 const Footer = () => {
--- src/components/home/SlantedBanner.js
+++ src/components/home/SlantedBanner.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import backgroundImage from '../../img/3187989.jpg'
-import SlantDivider from '../miscellaneous/SlantDivider';
+import { Parallax } from 'react-parallax';
 import ParticleEnv from './ParticleEnv';
 import './SlantedBanner.css';
 
 const SlantedBanner = () => {
   return (
     <div className='slantedbanner'>
-      <div className='banner-top'>
+        <Parallax blur={0} bgImage={backgroundImage} bgImageAlt="bg" strength={500}
+        bgImageSizes={1}>
+                <div className='banner-top'>
         <h1 className='banner-top-heading'>Gatlin Cooper</h1>
         <h2 className='banner-top-subheading'>Web Application Developer</h2>
         <div className='particles'>
           <ParticleEnv/>
+          </div>
         </div>
-        <img src={backgroundImage}alt="bg"></img>
+        </Parallax>
       </div>
-    </div>
   )
 }
 
