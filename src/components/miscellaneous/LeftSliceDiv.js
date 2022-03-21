52ac285cb1e9f071e1a681075c13f7efea07ea11
gjcooper99 <gjcooper99@gmail.com>
2022-03-20 20:34:45 -0700

    Styling and content inclusion for quick access links and resources

--- src/components/miscellaneous/LeftSliceDiv.js
+++ src/components/miscellaneous/LeftSliceDiv.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './LeftSliceDiv.css'
+import { GoMarkGithub } from "react-icons/go";
+import { BsLinkedin, BsFillJournalBookmarkFill } from "react-icons/bs"
 
 const LeftSliceDiv = () => {
   return (
 <div id="outer-left">
-    <div id="inner-left">Contact</div>
+    <div id="inner-left">
+    <div className='headingtext'>
+      Quick Access Links and Resources
+    </div>
+    <div className='link'>
+          <a href="https://github.com/cooperphyz">GitHub</a><GoMarkGithub size={70}/>
+        </div>
+        <div className='link'>
+          <a href="https://www.linkedin.com/in/gatlin-cooper/">Resume</a><BsFillJournalBookmarkFill size={70}/>
+        </div>
+        <div className='link'>
+          <a href="https://www.linkedin.com/in/gatlin-cooper/">LinkedIn</a><BsLinkedin size={70}/>
+        </div>
+    </div>
 </div>
   )
 }
