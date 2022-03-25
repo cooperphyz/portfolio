c4b2fb4b7ba564c56db52fadffad0b19a63d712b
gjcooper99 <gjcooper99@gmail.com>
2022-03-24 19:19:26 -0700

    Projects page base plate creation, change text on paragraph channel

--- src/components/miscellaneous/ParagraphContentPanel.js
+++ src/components/miscellaneous/ParagraphContentPanel.js
@@ -11,7 +11,7 @@ const ParagraphContentPanel = () => {
         </div>
         <div className='paragraph-text'>
             <p>I am a senior year computer science student studying at OSU-Cascades.</p>
-            <p>I love working with front-end frameworks, such as React.js and Ruby on Rails, to develop functional and practical web applications. </p>
+            <p>I love working with web application frameworks, particularly React.js and Ruby on Rails, to develop functional and practical web applications. </p>
             <p>Please feel free to browse the various sections of this website to learn more about me, my academics, and my projects.</p>
         </div>
       </div>
