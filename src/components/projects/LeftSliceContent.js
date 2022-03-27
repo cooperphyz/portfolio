60f7f85c580483dd9d523c77eb1d6d0baa29d53a
gjcooper99 <gjcooper99@gmail.com>
2022-03-26 21:29:29 -0700

    Formatted and styled left project slices

--- src/components/projects/LeftSliceContent.js
+++ src/components/projects/LeftSliceContent.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './LeftSliceContent.css'
+import ButtonMain from '../miscellaneous/ButtonMain'
+import { AiFillPlayCircle} from "react-icons/ai"
+import { BsCode} from "react-icons/bs"
 
 class LeftSliceDiv extends React.Component {
 
@@ -17,21 +20,27 @@ class LeftSliceDiv extends React.Component {
   }
 render() {
   return (
-  <div id="leftslice-outer-left"> 
+  <div id="leftslice-outer-left">
+    <div className='leftslice-img-container'>
+      <div className='fade'/>
+      <img className='leftslice-img' src='https://i.imgur.com/NYXCgK2.png' alt="bda"/>
+    </div>
     <div id="leftslice-inner-left">
     <div className='leftslice-headingtext'>
       {this.props.headingtext}
     </div>
-    <div className='tags'>
+    <div className='leftslice-tags'>
     {
       this.languages = this.props.languages.map((language) =>
       <p className='language-tag'>{language}</p>
       )
     }
     </div>
-    <div className='leftslice-img-container'>
-      <div className='fade'/>
-      <img className='leftslice-img' src='https://i.imgur.com/cb9Bvul.png'></img>
+    <div className='leftslice-sourcecode'>
+      <ButtonMain buttonlabel={"Code"} icon={<BsCode/>}/>
+    </div>
+    <div className='leftslice-project-live'>
+    <ButtonMain buttonlabel={"Live"} icon={<AiFillPlayCircle/>}/>
     </div>
     </div>
 </div>
