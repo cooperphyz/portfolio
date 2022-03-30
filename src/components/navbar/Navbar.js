4dc5fa58784d4daa99efe62ff602568ab3183078
gjcooper99 <gjcooper99@gmail.com>
2022-03-29 21:41:44 -0700

    Created and styled animated hamburger menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { GiHamburgerMenu } from "react-icons/gi"
+import { AiOutlineClose } from "react-icons/ai"
+import Fade from 'react-reveal/Fade';
 
 const Navbar = () => {
+
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () =>{
+    setHamburgerOpen(!hamburgerOpen)
+  }
+
   return (
     <div className='nav'>
       <nav className='navbar'>
@@ -15,8 +24,25 @@ const Navbar = () => {
         </ul>
       </nav>
       <div className='hamburger'>
-      <GiHamburgerMenu size={50} color={'white'}/>
+      { !hamburgerOpen ? <GiHamburgerMenu onClick={toggleHamburger} size={50} color={'white'}/> : null }
       </div>
+      <Fade right when={hamburgerOpen}>
+        <div className='hamburger-menu'>
+          <AiOutlineClose className='hamburger-close' onClick={toggleHamburger} size={50} color= {'white'}/>
+          <ul className='hamburger-items'>
+            <li className='hamburger-item'><a href="/">Home</a></li>
+            <hr/>
+            <li className='hamburger-item'><a href="/about">About</a></li>
+            <hr/>
+            <li className='hamburger-item'><a href="/projects">Projects</a></li>
+          </ul>
+        </div>
+      </Fade>
+      { hamburgerOpen ? 
+      <Fade when={hamburgerOpen}>
+        <div className='hamburger-background-blur'/> 
+      </Fade>
+      : null }
     </div>
   )
 }
